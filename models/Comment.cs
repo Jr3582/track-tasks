c0d66958f2fc5f28efd1ae70e221f7816c7e078c
JimmyRen <jimmyren590@gmail.com>
2026-08-23 18:55:23 -0400

    added front-end for comments, and UserToComment model

---
+++ models/Comment.cs
@@ -0,0 +1,8 @@
+public class Comment
+{
+    public int Id { get; set; }
+    public required string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int TaskId { get; set; }
+    public int UserId { get; set; }
+}
